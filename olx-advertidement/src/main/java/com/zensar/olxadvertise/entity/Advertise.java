471218e668a30501d839e68f5879f5d827f9e28c
TrigunsateeshPolireddy <p.trigunsateesh@zensar.com>
2022-05-06 18:34:12 +0530

    added service layer

--- olx-advertidement/src/main/java/com/zensar/olxadvertise/entity/Advertise.java
+++ olx-advertidement/src/main/java/com/zensar/olxadvertise/entity/Advertise.java
@@ -13,5 +13,29 @@ public class Advertise {
 	private long price;
 	private String description;
 	private int categoryId;
+	public Object getCreateddate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setUsername(Object username) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Object getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setStatus(Object status) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Object getCreateddate1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- olx-advertidement/src/main/java/com/zensar/olxadvertisement/OlxAdvertidementApplication.java
+++ olx-advertidement/src/main/java/com/zensar/olxadvertisement/OlxAdvertidementApplication.java
@@ -8,6 +8,7 @@ public class OlxAdvertidementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlxAdvertidementApplication.class, args);
+
 	}
 
 }
